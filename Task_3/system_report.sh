1f260e3830b0ddb1694c75d3b45196fb5b5b0dd8
sayali1203 <140812809+sayali1203@users.noreply.github.com>
2026-06-14 15:14:52 -0400

    Update system_report.sh

--- Task_3/system_report.sh
+++ Task_3/system_report.sh
@@ -3,10 +3,10 @@
 echo "System Information Report"
 echo "-------------------------"
 
-echo "User: $(whoami)"
-echo "Hostname: $(hostname)"
-echo "Date: $(date)"
-echo "Current Directory: $(pwd)"
+echo "User: kali"
+echo "Hostname: kali"
+echo "Date: Sun Jun 14 03:14:00 PM EDT 2026 "
+echo "Current Directory: /home/kali"
 
 echo ""
 echo "Memory Usage:"
